339b6a7d23f6fa4939e2099d8a3d10b77855ac3d
Josh Jacobson <joshjacobson14@gmail.com>
2021-06-21 15:38:56 -0700

    lecture work-in-progress

---
+++ hooks/src/components/Person.js
@@ -0,0 +1,35 @@
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+
+const Person = ({ person, isFetching, error }) => {
+  useEffect(() => {
+  });
+
+  if (error) {
+    return <h2>We got an error: {error}</h2>;
+  }
+
+  if (isFetching) {
+    return <h2>Fetching person for ya!</h2>;
+  }
+
+  return (
+    <>
+      <div>
+        <h2>Say Hi to: {person.name.first} {person.name.last}</h2>
+        <img src={person.picture.large}/>
+      </div>
+      <button>Get new person</button>
+    </>
+  );
+};
+
+const mapStateToProps = state => {
+  return {
+    person: state.person,
+    isFetching: state.isFetching,
+    error: state.error
+  };
+};
+
+export default connect(mapStateToProps)(Person);
---
+++ hooks/src/reducers/index.js
@@ -0,0 +1,23 @@
+const initialState = {
+  person: {
+    name: {
+      title: "Mr",
+      first: "Silas",
+      last: "Petersen"
+    },
+    picture: {
+      large: "https://randomuser.me/api/portraits/men/70.jpg",
+      medium: "https://randomuser.me/api/portraits/med/men/70.jpg",
+      thumbnail: "https://randomuser.me/api/portraits/thumb/men/70.jpg"
+    }
+  },
+  isFetching: false,
+  error: ''
+};
+
+export const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};

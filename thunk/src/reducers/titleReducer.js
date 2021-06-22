6e8fa8f3e93783f4df78b5d796be36ac5c98f0b8
Josh Jacobson <joshjacobson14@gmail.com>
2021-06-21 17:37:53 -0700

    lecture work-in-progress

---
+++ thunk/src/reducers/titleReducer.js
@@ -0,0 +1,11 @@
+const initialState = {
+    editing: false,
+    title: 'WEBPT28 Thunky Reducer fiesta'
+};
+
+export const titleReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case default:
+            return state;
+    }
+}

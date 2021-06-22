e2240298d38c30df069b40f230866f0e17ba6696
Josh Jacobson <joshjacobson14@gmail.com>
2021-06-21 18:09:44 -0700

    lecture work-in-progress

---
+++ thunk/src/actions/quoteActions.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+export const fetchQuote = () => {
+  // Hit the api (axios)
+
+  // Then, if it's happy, save the response to state.quoteReducer.quote
+
+  // If it's sad, catch the error message and save it to state.quoteReducer.error
+}

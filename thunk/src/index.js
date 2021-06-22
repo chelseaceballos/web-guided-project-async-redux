8e6a68828e332093c7e407f517e225995e278423
Josh Jacobson <joshjacobson14@gmail.com>
2021-06-21 17:43:28 -0700

    lecture work-in-progress

--- thunk/src/index.js
+++ thunk/src/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore } from "redux";
 
 import Title from "./components/Title";
+import { titleReducer } from "./reducers/titleReducer";
 import "./styles.css";
 
 // Step 1: create the Redux store
+const store = createStore(titleReducer);
+
 
 function App() {
   return (

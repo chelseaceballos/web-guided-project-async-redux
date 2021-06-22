9d8649a42052439c05f54879db1d555737327715
Josh Jacobson <joshjacobson14@gmail.com>
2021-06-21 18:40:27 -0700

    lecture work-in-progress

--- hooks/src/components/Quote.js
+++ hooks/src/components/Quote.js
@@ -3,9 +3,10 @@ import { useDispatch, useSelector } from "react-redux";
 import { fetchQuote } from "../actions/quoteActions";
 
 const Quote = (props) => {
-  useEffect(() => { fetchQuote(); }, []); // empty dep array for componentDidMount
   const { quote, loading } = useSelector((state) => state.quoteReducer);
   const dispatch = useDispatch();
+  useEffect(() => { fetchQuote(dispatch); }, []); // empty dep array for componentDidMount
+
 
 
   if (loading) {
@@ -14,13 +15,10 @@ const Quote = (props) => {
   return (
     <>
       <h2>Kanye once said: {quote} </h2>
-      <button onClick={fetchQuote} >New quote</button>
+      <button onClick={() => fetchQuote(dispatch)} >New quote</button>
     </>
   )
 }
 
 // Step 3: connect components (we can use React Redux hooks instead of connect if we'd like to, for function components only)
-
-
-
 export default Quote;

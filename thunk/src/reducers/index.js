e42b4b93ddb639b834e67324638da09199ca0f96
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-08-11 13:26:07 -0400

    lecture work-in-progress

--- thunk/src/reducers/index.js
+++ thunk/src/reducers/index.js
@@ -11,8 +11,8 @@ const initialState = {
       thumbnail: "https://randomuser.me/api/portraits/thumb/men/70.jpg"
     }
   },
-  isFetching: true,
-  error: ''
+  isFetching: false,
+  error: 'This is an error'
 };
 
 export const reducer = (state = initialState, action) => {
